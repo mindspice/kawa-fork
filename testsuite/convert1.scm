121be476a75e419725c35fb4c6f82bb13f5e2802
Per Bothner <per@bothner.com>
2019-05-11 16:02:45 -0700

    * convert1.scm, errors3.scm: Generalize error patterns for java 9+.

--- testsuite/convert1.scm
+++ testsuite/convert1.scm
@@ -12,10 +12,10 @@
  (format #t "should-fail i->c #\x123: ~w~%" (integer->char #\x123))
  (ex java.lang.ClassCastException
      (format #t "caught from i->c 123: ~w~%" ex)))
-;; Output-pattern: caught from i->c 123: java.lang.ClassCastException: .*gnu.text.Char cannot be cast to .*java.lang.Integer
+;; Output-pattern: caught from i->c 123: java.lang.ClassCastException: .*gnu.text.Char cannot be cast to .*java.lang.Integer.*
 
 (try-catch
  (format #t "should-fail c->i 120: ~w~%" (char->integer 120))
   (ex java.lang.ClassCastException
       (format #t "caught from c->i 120: ~w~%" ex)))
-;; Output: caught from c->i 120: java.lang.ClassCastException: gnu.math.IntNum cannot be cast to gnu.text.Char
+;; Output-pattern: caught from c->i 120: java.lang.ClassCastException: .*gnu.math.IntNum cannot be cast to .*gnu.text.Char.*
