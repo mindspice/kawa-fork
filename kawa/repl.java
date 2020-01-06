1b4ba1907e9760e7771e3575b278666aed882316
Per Bothner <per@bothner.com>
2020-01-06 11:56:49 -0800

    Update version number to 3.1 and copyright year to 2020.

--- kawa/repl.java
+++ kawa/repl.java
@@ -639,7 +639,7 @@ public class repl extends Procedure0or1 {
                 System.out.print("Kawa ");
                 System.out.print(Version.getVersion());
                 System.out.println();
-                System.out.println("Copyright (C) 2019 Per Bothner");
+                System.out.println("Copyright (C) 2020 Per Bothner");
                 something_done = true;
             } else if (arg.startsWith("-D")) {
                 int eq = arg.indexOf('=');
